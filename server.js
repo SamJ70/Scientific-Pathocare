f8610c9f76ac7a0ce76786720f9f118dba2d8e3a
SamJ70 <156401039+SamJ70@users.noreply.github.com>
2025-03-22 18:34:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -65,3 +65,7 @@ app.post("/send-email", async (req, res) => {
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+    res.send("Backend is running!");
+});
+
